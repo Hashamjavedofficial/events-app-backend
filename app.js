43827bcc38875ec9e9b4c6bd4a615089ee1768f1
Hasham Javed <hasham.hasham1@gmail.com>
2021-03-29 23:40:40 +0500

    route added to app file

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 const cors = require('cors')
 
+const eventRouter = require('./routes/event')
+
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+
+app.use('/events',eventRouter)
 
 const PORT =  process.env.PORT || 3000;
 
